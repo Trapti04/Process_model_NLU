c11b6888bdc8e20e1d4c8d7b29de2d398e02f6b8
Trapti04 <trapti.kalra@gmail.com>
2021-04-08 22:23:24 +0530

    base fubdtion definitions-inprogress

--- main/text_processing.py
+++ main/text_processing.py
@@ -2,17 +2,47 @@ import nltk
 import stanza
 
 #stanza.download('en') # This sets up a default neural pipeline in English. saved to C:\Users\user\stanza_resources.
-
-sample1 = 'Once the Senior General manager approves the invoice, then we can process it. It will  take 10 days after that to complete the reimbursement'
-print(sample1)
-file1 = open("sample_passages/car_insurance_claim_process.txt")
-sample_text = file1.readlines()
 nlp = stanza.Pipeline('en')
 
-doc = nlp(sample_text[0])
-for sentence in doc.sentences:
-    print(sentence.ents)
-for sentence in doc.sentences:
-    print(sentence.dependencies)
-print(*[f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}' for sent in doc.sentences for word in sent.words], sep='\n')
-print(*[f'id: {word.id}\tword: {word.text}\thead id: {word.head}\thead: {sent.words[word.head-1].text if word.head > 0 else "root"}\tdeprel: {word.deprel}' for sent in doc.sentences for word in sent.words], sep='\n')
+def tokenize_text(text):
+    doc = nlp(text)
+    return doc
+
+# test works for simple sentences (sentences which do not have subsentences only)
+def test():
+    file = open("sample_passages/car_insurance_claim_process.txt")
+    sample_text = file.readlines()
+    doc = tokenize_text(sample_text[0])
+    return doc, len(doc.sentences)
+
+def isPassive(sent):
+    passive_indicator = "Voice=Pass"
+    isPassive = False
+    for word in sent.words:
+        if word.feats:
+            if passive_indicator in word.feats:
+                isPassive = True
+    return isPassive           
+        
+def extract_elements(sent):
+    actors = []
+    rawActions= []
+    b_passive = isPassive(sent) 
+    actors = determineActors(b_passive,sent)
+    rawActions = determineActions(b_passive,sent)     
+    return b_passive  
+
+def determineActors(b_passive,sent):
+    result=[]
+    return result
+
+def determineActions(b_passive,sent):
+    result=[]
+    return result
+
+
+if __name__ == "__main__":
+    doc,s_count = test()
+    for i,sent in enumerate(doc.sentences):
+        print (i+1, extract_elements(sent), sent)
+    
